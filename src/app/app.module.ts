56b4ccbd6b87ab054b5423ac82e5d4d616af6a33
luisurruela <luis.urruela@gmail.com>
2019-10-10 20:41:26 -0500

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutModule } from 'angular-admin-lte';    //Loading layout module
+import { BoxModule } from 'angular-admin-lte';       //Box component
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    BoxModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
